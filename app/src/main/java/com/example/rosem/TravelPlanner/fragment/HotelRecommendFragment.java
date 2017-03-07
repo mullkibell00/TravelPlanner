ef5cfed31cf6d8bf701484bb3c68cdb5033d9d67
mullkibell00 <mullkibell00@gmail.com>
2017-03-07 20:38:08 +0900

    implement onCreate for recommendFrag and schedulingFrag

--- app/src/main/java/com/example/rosem/TravelPlanner/fragment/HotelRecommendFragment.java
+++ app/src/main/java/com/example/rosem/TravelPlanner/fragment/HotelRecommendFragment.java
@@ -1,6 +1,15 @@
 package com.example.rosem.TravelPlanner.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.rosem.TravelPlanner.R;
+import com.example.rosem.TravelPlanner.activity.CreatePlanActivity;
 
 /**
  * Created by rosem on 2017-03-07.
@@ -12,4 +21,42 @@ public class HotelRecommendFragment extends Fragment {
         HotelRecommendFragment fragment = new HotelRecommendFragment();
         return fragment;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.plan_hotel_recommend,container,false);
+
+
+
+
+        Button nextButton = (Button)getActivity().findViewById(R.id.create_plan_next);
+        Button prevButton = (Button)getActivity().findViewById(R.id.create_plan_prev);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                ((CreatePlanActivity)getActivity()).movePrev();
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                ((CreatePlanActivity)getActivity()).moveNext();
+            }
+        });
+        return view;
+    }
+
+    private void saveData()
+    {
+
+    }
+
 }
--- app/src/main/java/com/example/rosem/TravelPlanner/fragment/SchedulingFragment.java
+++ app/src/main/java/com/example/rosem/TravelPlanner/fragment/SchedulingFragment.java
@@ -1,6 +1,15 @@
 package com.example.rosem.TravelPlanner.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.rosem.TravelPlanner.R;
+import com.example.rosem.TravelPlanner.activity.CreatePlanActivity;
 
 /**
  * Created by rosem on 2017-03-07.
@@ -11,5 +20,42 @@ public class SchedulingFragment extends Fragment {
     {
         SchedulingFragment fragment = new SchedulingFragment();
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.plan_schedule,container,false);
+
+
+
+
+        Button nextButton = (Button)getActivity().findViewById(R.id.create_plan_next);
+        Button prevButton = (Button)getActivity().findViewById(R.id.create_plan_prev);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                ((CreatePlanActivity)getActivity()).movePrev();
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveData();
+                ((CreatePlanActivity)getActivity()).moveNext();
+            }
+        });
+        return view;
+    }
+
+    private void saveData()
+    {
+
     }
 }
