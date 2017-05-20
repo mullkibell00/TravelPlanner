58f96178031c23ed6a5183cde8235a54b2013b8a
mullkibell00 <mullkibell00@gmail.com>
2017-05-20 12:24:48 +0900

    로그인 액티비티 구현 완료 : 로그인 관련 스트링 및 레이아웃 추가

--- app/src/main/java/com/example/rosem/TravelPlanner/Activity/SignInActivity.java
+++ app/src/main/java/com/example/rosem/TravelPlanner/Activity/SignInActivity.java
@@ -1,11 +1,135 @@
 package com.example.rosem.TravelPlanner.Activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.rosem.TravelPlanner.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 /**
  * Created by bianca on 2017-05-20.
  */
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
+    private GoogleApiClient mGoogleClient;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static int GOOGLE_SIGN_IN = 1004;
+    private static int GOOGLE_SIGN_IN_CONNECTION_ERROR = 11;
+    private static int GOOGLE_SIGN_IN_FAIL = 12;
+    private static int GOOGLE_FIREBASE_AUTH_FAIL=13;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.google_wep_clinet_api_key)).build();
+
+        mGoogleClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+
+        findViewById(R.id.google_sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleClient);
+                startActivityForResult(intent,GOOGLE_SIGN_IN);
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null)
+                {
+                    Toast.makeText(getApplicationContext(), getString(R.string.login_success),Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), getString(R.string.logout_success),Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener!=null)
+        {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GOOGLE_SIGN_IN)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess())
+            {
+                GoogleSignInAccount account = result.getSignInAccount();
+                AuthFirebaseWithGoogle(account);
+            }
+            else
+            {
+                showFailedMsg(GOOGLE_SIGN_IN_FAIL);
+            }
+
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        showFailedMsg(GOOGLE_SIGN_IN_CONNECTION_ERROR);
+    }
+
+    private void showFailedMsg(int errorCode)
+    {
+        Toast.makeText(this, getString(R.string.login_failed)+errorCode, Toast.LENGTH_SHORT).show();
+    }
+
+    private void AuthFirebaseWithGoogle(GoogleSignInAccount account)
+    {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful())
+                        {
+                            showFailedMsg(GOOGLE_FIREBASE_AUTH_FAIL);
+                        }
+                    }
+                });
+    }
 }
