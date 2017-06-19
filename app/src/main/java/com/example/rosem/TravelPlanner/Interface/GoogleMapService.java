d5c6967242fa4f0fd4eff046480ca32dc6698fb3
Min Kyeong, Cho <mullkibell00@gmail.com>
2017-06-19 11:34:27 +0900

    getSiteInfo(String placeId, String lang, String key) :: Site(Place)의 대한 구글 맵의 정보를 가져온다 getRecommendHotelInfo(String location, int radius, String lang, String type, String key) :: 추천 호텔 리스트를 가져온다 getDistanceMatrix(String mode, String lang, String key) :: 거리 매트릭스를 가져온다

--- app/src/main/java/com/example/rosem/TravelPlanner/Interface/GoogleMapService.java
+++ app/src/main/java/com/example/rosem/TravelPlanner/Interface/GoogleMapService.java
@@ -4,7 +4,9 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Query;
 
 /**
  * Created by rosem on 2017-06-19.
@@ -12,7 +14,13 @@ import retrofit2.http.Headers;
 
 public interface GoogleMapService {
     @Headers( "Content-Type: application/json; charset=utf-8")
-    Call<ResponseBody> getSiteInfo();
+    @GET("/maps/api/geocode/json")
+    Call<ResponseBody> getSiteInfo(@Query("place_id")String placeId, @Query("language")String lang, @Query("key")String key);
+    @GET("/maps/api/place/nearbysearch/json")
+    Call<ResponseBody> getRecommendHotelInfo(@Query("location")String location, @Query("radius")int radius,
+                                             @Query("language")String lang, @Query("types")String type,@Query("key")String key );
+    @GET("/maps/api/distancematrix/json")
+    Call<ResponseBody> getDistanceMatrix(@Query("mode")String mode, @Query("language")String lang, @Query("key")String key);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://maps.googleapis.com")
